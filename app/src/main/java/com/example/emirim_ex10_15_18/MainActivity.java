969409da1476d7bc46b420b2e7d780869694a498
dana8 <w2008@e-mirim.hs.kr>
2021-07-20 13:24:11 +0900

    Edit RadioButton MainActivity Commit

--- app/src/main/java/com/example/emirim_ex10_15_18/MainActivity.java
+++ app/src/main/java/com/example/emirim_ex10_15_18/MainActivity.java
@@ -8,11 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1;
     EditText edit2;
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +24,37 @@ public class MainActivity extends AppCompatActivity {
 
         edit1 = findViewById(R.id.edit1);
         edit2 = findViewById(R.id.edit2);
-        Button btnPlus = findViewById(R.id.btn_plus);
+        rg = findViewById(R.id.rg);
 
-        btnPlus.setOnClickListener(btnListener);
+        Button btnCalc = findViewById(R.id.btn_calc);
+
+        btnCalc.setOnClickListener(btnListener);
     }
 
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            char op = 0;
+
+            switch (rg.getCheckedRadioButtonId()){
+                case R.id.r_plus:
+                    op = '+';
+                    break;
+                case R.id.r_minus:
+                    op = '-';
+                    break;
+                case R.id.r_multi:
+                    op = '*';
+                    break;
+                case R.id.r_div:
+                    op = '/';
+                    break;
+            }
+
             Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
             intent.putExtra("num1", Integer.parseInt(edit1.getText().toString()));
             intent.putExtra("num2", Integer.parseInt(edit2.getText().toString()));
+            intent.putExtra("op", op);
             startActivityForResult(intent,0);
         }
     };
